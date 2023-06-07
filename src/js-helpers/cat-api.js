316b30a7283bb875eed0d196e58a7468e6def3c8
Ivan Bilobrov <ivanbilobrov07@gmail.com>
2023-06-07 17:47:14 +0300

    Change on async/await

--- src/js-helpers/cat-api.js
+++ src/js-helpers/cat-api.js
@@ -2,26 +2,26 @@ import { params } from './common';
 
 const { BASE_URL, API_KEY, ENDPOINT } = params;
 
-const fetchBreeds = () => {
-  return fetch(`${BASE_URL}${ENDPOINT}?api_key=${API_KEY}`).then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
+const fetchBreeds = async () => {
+  const response = await fetch(`${BASE_URL}${ENDPOINT}?api_key=${API_KEY}`);
 
-    return response.json();
-  });
+  if (!response.ok) {
+    throw new Error(response.status);
+  }
+
+  return response.json();
 };
 
-const fetchCatByBreed = breedId => {
-  return fetch(`${BASE_URL}/images/${breedId}?api_key=${API_KEY}`).then(
-    response => {
-      if (!response.ok) {
-        throw new Error(response.status);
-      }
-
-      return response.json();
-    }
+const fetchCatByBreed = async breedId => {
+  const response = await fetch(
+    `${BASE_URL}/images/${breedId}?api_key=${API_KEY}`
   );
+
+  if (!response.ok) {
+    throw new Error(response.status);
+  }
+
+  return response.json();
 };
 
 export { fetchBreeds, fetchCatByBreed };
